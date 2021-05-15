306e20b1d23d279ecab5d806727879cc865f2568
Bytes Of Pi <44814181+BytesOfPi@users.noreply.github.com>
2021-05-15 10:26:22 -0400

    first commit

---
+++ src/main/java/com/bytesofpi/apcs/exam2021/WordMatch.java
@@ -0,0 +1,44 @@
+package com.bytesofpi.apcs.exam2021;
+
+public class WordMatch
+    {
+
+    private String secret;
+
+    public WordMatch( String secret )
+        {
+        super();
+        this.secret = secret;
+        }
+
+    /**
+     * Returns the better of two guesses, as determined by scoreGuess and the rules
+     * for a tie-breaker that are described in part (b)
+     *
+     * Preconditions: guess1 and guess2 contain all lowercase letters guess1 is not
+     * the same as guess2
+     *
+     * @param guess1
+     * @param guess2
+     * @return
+     */
+    public String findBetterGuess( String guess1, String guess2 )
+        {
+        /* To be implemented in part (b) */
+        }
+
+    /**
+     * Returns the score for guess, as described in part (a)
+     *
+     * Preconditions: 0 < guess.length() <= secret.length()
+     *
+     * @param guess1
+     * @param guess2
+     * @return
+     */
+    public int scoreGuess( String guess )
+        {
+        /* To be implemented in part (a) */
+        }
+
+    }
---
+++ src/main/java/com/bytesofpi/apcs/exam2021/_RunQuestion04.java
@@ -0,0 +1,62 @@
+package com.bytesofpi.apcs.exam2021;
+
+public class _RunQuestion04
+    {
+
+    public static void main( String[] args )
+        {
+
+        runPartA();
+
+        runPartB();
+
+        }
+
+    public static void runPartA()
+        {
+        System.out.println( "------------------------------------------------------" );
+        System.out.println( "Question 1 (a)" );
+        System.out.println();
+        WordMatch wm = new WordMatch( "mississippi" );
+        printQ1( "i", wm );
+        printQ1( "iss", wm );
+        printQ1( "issipp", wm );
+        printQ1( "mississippi", wm );
+
+        WordMatch game = new WordMatch( "aaaabb" );
+        printQ1( "a", game );
+        printQ1( "aa", game );
+        printQ1( "aaa", game );
+        printQ1( "aabb", game );
+        printQ1( "c", game );
+        }
+
+    public static void printQ1( String guess, WordMatch wm )
+        {
+        System.out.println( String.format( "Guess [%s] Val [%d] ", guess, wm.scoreGuess( guess ) ) );
+        }
+
+    private static void runPartB()
+        {
+        System.out.println( "------------------------------------------------------" );
+        System.out.println( "Question 1 (b)" );
+        System.out.println();
+        WordMatch game = new WordMatch( "concatenation" );
+
+        printQ2( "ten", "nation", game );
+        printQ2( "con", "cat", game );
+
+        System.out.println();
+
+        }
+
+    public static void printQ2( String guess1, String guess2, WordMatch wm )
+        {
+        System.out.println(
+                String.format( "Guess1 Score [%s][%d] Guess1 Score [%s][%d] Best [%s] ",
+                        guess1, wm.scoreGuess( guess1 ),
+                        guess2, wm.scoreGuess( guess2 ),
+                        wm.findBetterGuess( guess1, guess2 ) ) );
+        }
+
+    }
